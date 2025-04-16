4e899b11cee9b03d0f656e11047ce6f48ef242fe
jaipipor <53977871+jaipipor@users.noreply.github.com>
2025-04-16 13:54:18 +0200

    File update

--- Python/setup.py
+++ Python/setup.py
@@ -1,9 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="o25",  # PyPI package name
-    version="0.1.0",
-    packages=find_packages(),  # Auto-finds 'Python' as package
-    package_dir={"": "."},     # Look in root directory
-    install_requires=["numpy", "scipy"],
+    name='o25',
+    version='0.1.0',
+    author='Jaime Pitarch',
+    author_email='jaime.pitarch@cnr.it',
+    description='Bidirectional correction and IOP retrieval for aquatic optics',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jaipipor/O25',
+    license='GPL-3.0',
+    packages=find_packages(where='Python'),
+    package_dir={'': 'Python'},
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
