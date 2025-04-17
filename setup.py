d9041d6aa5ce4e13858f54564b8e7e31442ac393
Jaime Pitarch <53977871+jaipipor@users.noreply.github.com>
2025-04-17 23:27:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="o25",
-    version="0.1.0",
+    version="0.1.8",
     author="Jaime Pitarch",
     author_email="jaime.pitarch@cnr.it",
     description="Bidirectional correction and IOP retrieval for aquatic optics",
